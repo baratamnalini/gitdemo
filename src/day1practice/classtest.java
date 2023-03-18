9ab0c9b82d2c2b5b6e58df21ce5ebf78446c2526
nalini <baratam.nalini@gmail.com>
2023-03-18 16:51:51 +0530

    first commit

---
+++ src/day1practice/classtest.java
@@ -0,0 +1,10 @@
+package day1practice;
+
+public class classtest {
+
+	public static void main(String[] args) {
+	System.out.println("nalini");
+
+	}
+
+}
---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author NaliniP
+ *
+ */
+module day1practice {
+}
